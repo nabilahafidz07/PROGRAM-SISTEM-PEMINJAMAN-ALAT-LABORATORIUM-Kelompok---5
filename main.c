9907fed2e95c1d9177280a6cc98af920ac5f199f
nabilahafidz07-source <nabilahafidz07@gmail.com>
2026-04-11 14:18:52 +0700

    pp

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int login(char role[]);
+void menuAdmin();
+void menuUser();
+
+int main(int argc, char *argv[]) {
+    char role[10];
+    int status;
+    int def;
+
+    status = login(role);
+
+    if (status == 0) {
+        printf("Login gagal!\n");
+        return 0;
+    }
+
+    // cek role
+    if (strcmp(role, "admin") == 0) {
+        menuAdmin();
+    } else {
+        menuUser();
+    }
+
+    return 0;
+}
+
+
 
